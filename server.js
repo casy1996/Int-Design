39e9bc66fb5d2fa220a71f74cbc00b1939d68e99
Christopher Sy <christophersy@Christophers-Laptop.local>
2024-06-16 21:33:41 -0700

    Commit 4 - Starter dependencies added, create mongo connection and port listerner.

--- server.js
+++ server.js
@@ -1,1 +1,38 @@
-//Dependencies
+//DEPENDENCIES
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const port = process.env.PORT || 4000;
+const mongoURI = 'mongodb://127.0.0.1:27017/gallery'
+
+//MIDDLEWARE
+
+//MongoDB CONNECTION
+async function connectToMongo() {
+    try {
+        await mongoose.connect(mongoURI)
+        console.log(`Connected to MongoDB!`)
+    } catch (error) {
+        console.error(error)
+    }
+}
+connectToMongo();
+
+//INDEX
+
+//NEW
+
+//DELETE
+
+//UPDATE
+
+//CREATE
+
+//EDIT
+
+//SHOW
+
+//EXPRESS WEB SERVER
+app.listen(port, ()=>{
+    console.log(`App is running on ${port}`)
+});
