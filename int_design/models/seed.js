0c592a426ed1e359d320d2c52887030f10a85ce1
Christopher Sy <christophersy@Christophers-Laptop.local>
2024-06-19 20:16:43 -0700

    Commit 12 - Begin refractoring routes to async try/catch. Index view no longer calls seed data but mongodb. Test in node successful

--- int_design/models/seed.js
+++ int_design/models/seed.js
@@ -5,7 +5,6 @@ const seedData = [
         image: 'https://i.postimg.cc/RqrBBnz5/IMG-4104-auto-x2-ca48ad6e-c001-475c-a32b-87611201a6fb.jpg',
         description: 'Statement chair to draw interest into any space. Bubble cushions bring a welcoming cozy feel',
         tags: ['statement','sofa','chair','bubble'],
-        // sold: false,
     },
     {
         name: 'Ettore Sottsass Mirror',
@@ -13,7 +12,6 @@ const seedData = [
         image: 'https://i.postimg.cc/5yQXgvmZ/decorative-objects-floor-mirrors-1970-1979-mid-century-modern-poltronova-living-in-style-gallery-273.jpg',
         description: 'Viral wavey standing mirror. Fill your space with the soft glow from the LED frame.',
         tags: ['statement','bold','backlit','retro','mirror'],
-        // sold: true,
     },
     {
         name: 'Lego Coffee Table',
@@ -21,7 +19,6 @@ const seedData = [
         image: 'https://i.postimg.cc/FR7btYZx/ff9b636f8bae5fd9655b6b67f01eacb1.jpg',
         description: 'Homemade coffee table with base made of Lego drawers. Heavy glass table top.',
         tags: ['lego','table','colorful'],
-        // sold: false,
     }
 ];
 
--- int_design/server.js
+++ int_design/server.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override')
 const app = express();
 const port = process.env.PORT || 4000;
+
+//CONFIGURATION
 const mongoURI = 'mongodb://127.0.0.1:27017/gallery'
 
 //MODELS
@@ -28,12 +30,62 @@ async function connectToMongo() {
 }
 connectToMongo();
 
+//DEFAULT DATA FOR DB
+
+// const defaultItems = [
+//     {
+//         name: 'Orb Sofa',
+//         price: 3578,
+//         image: 'https://i.postimg.cc/RqrBBnz5/IMG-4104-auto-x2-ca48ad6e-c001-475c-a32b-87611201a6fb.jpg',
+//         description: 'Statement chair to draw interest into any space. Bubble cushions bring a welcoming cozy feel',
+//         tags: ['statement','sofa','chair','bubble'],
+//     },
+//     {
+//         name: 'Ettore Sottsass Mirror',
+//         price: 999,
+//         image: 'https://i.postimg.cc/5yQXgvmZ/decorative-objects-floor-mirrors-1970-1979-mid-century-modern-poltronova-living-in-style-gallery-273.jpg',
+//         description: 'Viral wavey standing mirror. Fill your space with the soft glow from the LED frame.',
+//         tags: ['statement','bold','backlit','retro','mirror'],
+//     },
+//     {
+//         name: 'Lego Coffee Table',
+//         price: 500,
+//         image: 'https://i.postimg.cc/FR7btYZx/ff9b636f8bae5fd9655b6b67f01eacb1.jpg',
+//         description: 'Homemade coffee table with base made of Lego drawers. Heavy glass table top.',
+//         tags: ['lego','table','colorful'],
+//     }
+// ]
+
+// async function insertDefaultToGallery(defaultItems) {
+//     try {
+//        const baseGallery = await Gallery.insertMany(defaultItems);
+//        console.log(baseGallery)
+//     } catch (err) {
+//         console.error(err)
+//     } finally {
+//         mongoose.connection.close();
+//     }
+// }
+
+// insertDefaultToGallery(defaultItems)
+
+
 //INDEX
-app.get("/gallery", (req,res)=> {
+app.get("/gallery", async (req,res)=> {
     // res.send(seedData)
-    res.render("index.ejs", {
-        seedData: seedData,
-    })
+    // res.render("index.ejs", {
+    //     seedData: seedData,
+    // })
+    try {
+        const showAll = await Gallery.find({})
+        // console.log(showAll)
+        res.render("index.ejs", {
+            gallery: showAll,
+        })
+    } catch (error) {
+        console.error(err)
+        res.status(500).send(err)
+    }
 });
 
 //NEW
