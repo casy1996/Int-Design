5fbf3d2700be467057a8892a96e81e1046ab9b3e
Christopher Sy <christophersy@Christophers-Laptop.local>
2024-06-26 14:37:30 -0700

    Commit 36 - Added .env

--- int_design/models/seed.js
+++ int_design/models/seed.js
@@ -1,8 +1,8 @@
 const seedData = [
     {
         name: 'Orb Sofa',
-        price: 3578,
-        image: 'https://i.postimg.cc/RqrBBnz5/IMG-4104-auto-x2-ca48ad6e-c001-475c-a32b-87611201a6fb.jpg',
+        price: 1199,
+        image: 'https://i.postimg.cc/Vs4DcMkC/Bubble-Sofa-Armchair-Replica-Hero-scaled.webp',
         description: 'Statement chair to draw interest into any space. Bubble cushions bring a welcoming cozy feel',
         tags: ['statement','sofa','chair','bubble'],
     },
@@ -19,7 +19,42 @@ const seedData = [
         image: 'https://i.postimg.cc/FR7btYZx/ff9b636f8bae5fd9655b6b67f01eacb1.jpg',
         description: 'Homemade coffee table with base made of Lego drawers. Heavy glass table top.',
         tags: ['lego','table','colorful'],
-    }
+    },
+    {
+        name: 'Kinto Amber Glassware',
+        price: 120,
+        image: 'https://i.postimg.cc/FR7btYZx/ff9b636f8bae5fd9655b6b67f01eacb1.jpg',
+        description: 'Famouse japanese glassware from KINTO. Amber/sepia tone mugs, glasses, etc.',
+        tags: ['sepia','glassware','japan'],
+    },
+    {
+        name: 'Between Light and Dark',
+        price: 1399,
+        image: 'https://i.postimg.cc/zf94KhgB/skullpanda-BLAD.jpg',
+        description: 'Iconic popmart character SKULLPANDA version, Between Light and Dark.',
+        tags: ['popmart','skullpanda','moody'],
+    },
+    {
+        name: 'Akari A9 Lamp',
+        price: 550, 
+        image: 'https://i.postimg.cc/GhFMSNsb/isamunoguchi-akari.webp',
+        description: 'The Akari 9A is one of many environment changing mood setting light sculptures from the iconic Isamu Noguchi',
+        tags: ['paper','lantern','mood','art'],
+    },
+    {
+        name: 'Chunky Dinnerware & Spiral Stand',
+        price: 245,
+        image: 'https://i.postimg.cc/NFzghMnr/gustafwestman-chunky.jpg',
+        description: 'Gustaf Westmans viral chunky dinnerware paired with the multipurpose spiral stand.',
+        tags: ['ceramic','swedish','colorful'],
+    },
+    {
+        name: 'Miffy & Friends',
+        price: 80,
+        image: 'https://i.postimg.cc/HsdMQLHp/miffy-friends.webp',
+        description: 'Miffy & Friends crochet plushies. Miffy is a world renowned dutch mascot created by the late Dick Bruna.',
+        tags: ['crochet','plush','cute'],
+    },
 ];
 
 module.exports = seedData
--- int_design/server.js
+++ int_design/server.js
@@ -3,12 +3,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override')
 const app = express();
+require('dotenv').config();
 const port = process.env.PORT || 4000;
 const galleryController = require("./controllers/gallery.js")
 const favoriteController = require("./controllers/favorites.js")
 
+
 //CONFIGURATION
-const mongoURI = 'mongodb://127.0.0.1:27017/gallery'
+// const mongoURI = 'mongodb://127.0.0.1:27017/gallery'
+const mongoURI = process.env.MONGOURI
 
 //MMIDDLEWARE
 app.use(express.json());
@@ -22,12 +25,11 @@ app.use(favoriteController)
 async function connectToMongo() {
     try {
         await mongoose.connect(mongoURI)
-        console.log(`Connected to MongoDB!`)
+        console.log("Connected to MongoDB!")
     } catch (error) {
         console.error(error)
     }
 }
-
 connectToMongo();
 
 //EXPRESS WEB SERVER
